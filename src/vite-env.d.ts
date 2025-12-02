b22a4c701ccc7a0b0175d5605fa5c16d97bf3397
bylo-valutazioni <profili.bylo@gmail.com>
2025-12-02 15:33:15 +0100

    Add vite-env.d.ts for TypeScript

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_SUPABASE_TRILLO_URL: string;
+    readonly VITE_SUPABASE_TRILLO_ANON_KEY: string;
+    readonly VITE_SUPABASE_CONTO_URL: string;
+    readonly VITE_SUPABASE_CONTO_ANON_KEY: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
